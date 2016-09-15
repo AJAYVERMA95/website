7332705546fae32d1c88294e1b3690d25d45355c
AJAY VERMA <verma.av.ajay@gmail.com>
2016-09-15 15:52:48 +0530

    server.js added.server is up and running.

--- website/app.js
+++ website/app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/router.js');
+var users = require('./routes/users.js');
 
 var app = express();
 
@@ -15,12 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public','TIC','images','icon', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public','TIC')));
 
 app.use('/', routes);
 app.use('/users', users);
---
+++ website/server.js
@@ -0,0 +1,5 @@
+var app = require('./app.js');
+const PORT = 8080;
+app.listen(PORT,function () {
+	console.log("server up and running.");
+});
